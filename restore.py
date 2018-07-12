77aeba9e08d3b98b959ce37bf06b53eee3508502
THCKwarter <3734237+THCKwarter@users.noreply.github.com>
2018-07-11 22:22:45 -0400

    Added files

---
+++ restore.py
@@ -0,0 +1,29 @@
+import cv2
+
+# Takes in an input video and coverts it to a sequence of png images.
+#currentFrame = 0
+#vid = cv2.VideoCapture("input.mp4")
+
+#while(True):
+    # Capture frame-by-frame
+#    ret, frame = vid.read()
+
+    # Saves image of the current frame in png file
+    # png is used for its lossless quality
+#    name = './output/frame' + str(currentFrame) + '.png'
+#    print ('Creating...' + name)
+#    cv2.imwrite(name, frame)
+
+    # To stop duplicate images
+#    currentFrame += 1
+
+# When everything is done, release the capture
+#vid.release()
+#cv2.destroyAllWindows()
+
+# Take an image and apply a cleaning filter to it.
+videoName = input('Enter the file name of the video including the file extension (myvideo.mp4): ')
+print(framerate)
+
+framerate = input('Enter the frame rate of the video: ')
+print(framerate)
